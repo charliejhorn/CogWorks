693f562835ae5046f6ef6953ac70eae363f760e3
Charlie Horn <chazzahorn@gmail.com>
2025-11-05 15:58:18 +1100

    fresh repo, deleted everything

--- apps/old-frontend/src/components/index.jsx
+++
@@ -1,6 +0,0 @@
-// This file exports various React components used throughout the application.
-
-export { default as Header } from './Header';
-export { default as Footer } from './Footer';
-export { default as WorkshopCard } from './WorkshopCard';
-export { default as WorkshopForm } from './WorkshopForm';

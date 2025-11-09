df46e9e91e6176d4d2ee5a6bcf08bd1ec45e7a84
lily <haquynhchi@gmail.com>
2025-11-09 16:26:09 +1100

    Added name to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_11_08_042244) do
+ActiveRecord::Schema[8.0].define(version: 2025_11_09_052043) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -69,6 +69,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_11_08_042244) do
     t.string "password_hash"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
   end
 
   add_foreign_key "bikes", "customers"

1d9d059d666e8ab3dc8116fbebb00897d2bd062d
lily <haquynhchi@gmail.com>
2025-11-09 16:45:37 +1100

    Added user name to expected params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -76,6 +76,6 @@ class UsersController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def user_params
-        params.expect(user: %i[email password_hash])
+        params.expect(user: %i[name email password_hash])
     end
 end
